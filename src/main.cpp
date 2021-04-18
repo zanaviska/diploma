7393b8d5f4640d34a8bafd11c2c8ea24927a492c
András <dlink.uaua@gmail.com>
2021-04-18 16:43:01 +0300

    removed ML functions

--- src/main.cpp
+++ src/main.cpp
@@ -11,85 +11,10 @@
 #include <functions.h>
 #include <terminal.h>
 
-// Remove the bounding boxes with low confidence using non-maxima suppression
-void postprocess(cv::Mat &frame, const std::vector<cv::Mat> &outs)
-{
-    float confThreshold = 0.5; // Confidence threshold
-    float nmsThreshold = 0.4;  // Non-maximum suppression threshold
-    std::vector<int> classIds;
-    std::vector<float> confidences;
-    std::vector<cv::Rect> boxes;
-
-    for (size_t i = 0; i < outs.size(); ++i)
-    {
-        // Scan through all the bounding boxes output from the network and keep only the
-        // ones with high confidence scores. Assign the box's class label as the class
-        // with the highest score for the box.
-        float *data = (float *)outs[i].data;
-        for (int j = 0; j < outs[i].rows; ++j, data += outs[i].cols)
-        {
-            cv::Mat scores = outs[i].row(j).colRange(5, outs[i].cols);
-            cv::Point classIdPoint;
-            double confidence;
-            // Get the value and location of the maximum score
-            minMaxLoc(scores, 0, &confidence, 0, &classIdPoint);
-            //            if (confidence > confThreshold)
-            {
-                int centerX = (int)(data[0] * frame.cols);
-                int centerY = (int)(data[1] * frame.rows);
-                int width = (int)(data[2] * frame.cols);
-                int height = (int)(data[3] * frame.rows);
-                int left = centerX - width / 2;
-                int top = centerY - height / 2;
-
-                classIds.push_back(classIdPoint.x);
-                confidences.push_back((float)confidence);
-                boxes.push_back(cv::Rect(left, top, width, height));
-            }
-        }
-    }
-
-    // Perform non maximum suppression to eliminate redundant overlapping boxes with
-    // lower confidences
-    std::vector<int> indices;
-    cv::dnn::NMSBoxes(boxes, confidences, confThreshold, nmsThreshold, indices);
-    std::cout << indices.size();
-    for (size_t i = 0; i < indices.size(); ++i)
-    {
-        int idx = indices[i];
-        cv::Rect box = boxes[idx];
-        cv::rectangle(frame, box,
-                      cv::Scalar(255, 255 * (classIds[idx] == 2), 255 * (classIds[idx] == 1)), 2);
-        std::cout << '\n' << classIds[idx] << '\n';
-        //        drawPred(classIds[idx], confidences[idx], box.x, box.y, box.x + box.width,
-        //                 box.y + box.height, frame);
-    }
-}
-
-// Get the names of the output layers
-std::vector<std::string> getOutputsNames(const cv::dnn::Net &net)
-{
-    static std::vector<std::string> names;
-    if (names.empty())
-    {
-        // Get the indices of the output layers, i.e. the layers with unconnected outputs
-        std::vector<int> outLayers = net.getUnconnectedOutLayers();
-
-        // get the names of all the layers in the network
-        std::vector<std::string> layersNames = net.getLayerNames();
-
-        // Get the names of the output layers in names
-        names.resize(outLayers.size());
-        for (size_t i = 0; i < outLayers.size(); ++i)
-            names[i] = layersNames[outLayers[i] - 1];
-    }
-    return names;
-}
-
 int main(int argc, char **argv)
 {
     // Loads an image
-    cv::Mat src = cv::imread("../flowchart2.jpg", cv::IMREAD_GRAYSCALE);
+    cv::Mat src = cv::imread("../flowchart1.jpg", cv::IMREAD_GRAYSCALE);
     // Check if image is loaded fine
     if (src.empty())
     {
